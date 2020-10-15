f2f6f4b160431038f4285048677a898c97e5b551
andryue <kacperwojak17@gmail.com>
2020-10-15 12:26:47 +0200

    complete register function - handle errors etc

--- app/src/main/java/com/example/medled/authentication/Authentication.kt
+++ app/src/main/java/com/example/medled/authentication/Authentication.kt
@@ -4,22 +4,31 @@ import android.app.AlertDialog
 import android.view.View
 import com.example.medled.helpers.Helpers
 import com.google.firebase.auth.FirebaseAuth
+import java.lang.Exception
 
 class Authentication {
     private val auth = FirebaseAuth.getInstance()
     private val helpers: Helpers = Helpers()
 
+    //----------------------| Register With email and password |----------------------------
     fun registerWithEmailAndPassword(email: String, password: String,view: View, complete : () -> Unit) {
-        val dialog :AlertDialog = helpers.getDialog(view.context,"Registering")
-        dialog.show()
-        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
-            if (task.isSuccessful){
-
-                complete()
-            }else{
-
+        val dialog :AlertDialog = helpers.getLoadingDialog(view.context,"Registering")  //loading dialog
+        try {
+            dialog.show()
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful){
+                    complete()
+                }else{
+                    //catch network exception etc
+                    helpers.showSnackBar(task.exception!!.message.toString(),view)
+                }
+                dialog.dismiss()
             }
+        }catch (ex:Exception){
             dialog.dismiss()
+            helpers.showSnackBar(ex.message.toString(),view)
         }
+
     }
+    //============================================================================================
 }
--- app/src/main/java/com/example/medled/helpers/Helpers.kt
+++ app/src/main/java/com/example/medled/helpers/Helpers.kt
@@ -4,12 +4,19 @@ import android.app.Activity
 import android.app.AlertDialog
 import android.app.ProgressDialog
 import android.content.Context
+import android.view.View
 import com.example.medled.R
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_register.*
 
 class Helpers {
 
-    fun getDialog(context: Context,title:String) : AlertDialog {
-        return AlertDialog.Builder(context).setView(R.layout.progress_dialog_layout).setTitle(title).setCancelable(false).create()
+    fun getLoadingDialog(context: Context, title: String): AlertDialog {
+        return AlertDialog.Builder(context).setView(R.layout.progress_dialog_layout).setTitle(title)
+            .setCancelable(false).create()
     }
+
+    fun showSnackBar(message:String, view: View) = Snackbar.make(view,message,Snackbar.LENGTH_SHORT).show()
+
+
 }
