8298dba7c703346efff027f7580f31a41be0e8a9
andryue <kacperwojak17@gmail.com>
2020-10-20 10:11:47 +0200

    setup avatars in views

--- app/src/main/java/com/example/medled/screens/profile/ChooseAvatarFragment.kt
+++ app/src/main/java/com/example/medled/screens/profile/ChooseAvatarFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import com.example.medled.R
 import kotlinx.android.synthetic.main.fragment_choose_avatar.*
 
@@ -20,14 +21,44 @@ class ChooseAvatarFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         setupNavigation()
+        setupImages()
     }
 
-    private fun setupNavigation(){
+    private fun setupNavigation() {
         changeAvatarBackButton.setOnClickListener {
             requireActivity().onBackPressed()
         }
         saveAvatarButton.setOnClickListener {
             requireActivity().onBackPressed()
         }
+    }
+
+
+    private fun setupImages() {
+        val listOfAvatarsImageViews = arrayListOf<ImageView>(
+            avatar1, avatar2, avatar3, avatar4, avatar5, avatar6
+        )
+        val listOfDoctorsImages = arrayListOf<Int>(
+            R.drawable.doctor_avatar_1,
+            R.drawable.doctor_avatar_2,
+            R.drawable.doctor_avatar_3,
+            R.drawable.doctor_avatar_4,
+            R.drawable.doctor_avatar_5,
+            R.drawable.doctor_avatar_6
+        )
+
+        val listOfPatientsImages = arrayListOf<Int>(
+            R.drawable.user_avatar_1,
+            R.drawable.user_avatar_2,
+            R.drawable.user_avatar_3,
+            R.drawable.user_avatar_4,
+            R.drawable.user_avatar_5,
+            R.drawable.user_avatar_6
+        )
+
+        for(i in 0 until listOfAvatarsImageViews.size){
+            listOfAvatarsImageViews[i].setImageResource(if(false) listOfDoctorsImages[i] else listOfPatientsImages[i])
+        }
+
     }
 }
