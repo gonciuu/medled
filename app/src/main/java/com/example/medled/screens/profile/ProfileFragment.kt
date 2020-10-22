9e82d1942947ccde5b2692434b01a311aaa59b21
andryue <kacperwojak17@gmail.com>
2020-10-22 12:04:01 +0200

    setup user info in profile fragment

--- app/src/main/java/com/example/medled/screens/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/medled/screens/profile/ProfileFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.medled.R
 import com.example.medled.authentication.Authentication
+import com.example.medled.view_models.CurrentUserViewModel
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_profile.*
 
@@ -30,5 +34,21 @@ class ProfileFragment : Fragment() {
             Authentication().signOutFromFirebase(requireView())
         }
 
+        setupUserInfo()
+
     }
+
+    //-------------------------| Setup user info in profile fragment |---------------------------------
+    private fun setupUserInfo(){
+        val currentUserViewModel: CurrentUserViewModel = ViewModelProvider(requireActivity()).get(CurrentUserViewModel::class.java)
+        currentUserViewModel.getUser().observe(viewLifecycleOwner, Observer { user->
+            if(user!=null){
+                currentUserName.text = user.name
+                currentUserAvatar.setImageResource(user.avatar)
+                currentUserBio.text = if(user.isDoctor) user.medicineBranch else user.bio
+            }
+
+        })
+    }
+    //==================================================================================================
 }
